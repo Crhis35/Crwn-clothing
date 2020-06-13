48edb6a0dba0876b785c6a55178929b4f8daf8a3
Crhistian Caraballo <cristianjcj35@gmail.com>
2020-06-13 18:11:15 -0500

    Implement firebase utilities, include hability to authenticate

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,30 @@ const config = {
   appId: "1:12532908477:web:f1639538da193ffc38d2d4",
   measurementId: "G-Z95EY8K126",
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const creadAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        creadAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("Error creating user", error.message);
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
