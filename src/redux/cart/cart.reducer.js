23ca60422a0148533fe8dc0e1487c69f5ef5b543
Crhistian Caraballo <cristianjcj35@gmail.com>
2020-06-15 12:17:00 -0500

    Remove state

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,4 @@
-import CartActionTypes from './cart.types';
+import CartActionsTypes from './cart.types';
 import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
@@ -8,15 +8,22 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case CartActionTypes.TOGGLE_CART_HIDDEN:
+    case CartActionsTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
         hidden: !state.hidden,
       };
-    case CartActionTypes.ADD_ITEM:
+    case CartActionsTypes.ADD_ITEM:
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case CartActionsTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
       };
     default:
       return state;
